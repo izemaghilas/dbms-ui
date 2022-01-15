4968e90db9e643962e668d9595f21e07da392d3d
izemaghilas <saidtala23@gmail.com>
2022-01-15 21:33:51 +0100

    set api url

--- src/api-client/HttpRequest.ts
+++ src/api-client/HttpRequest.ts
@@ -1,10 +1,9 @@
 import IHttpRequest from "./interfaces/IHttpRequest";
 
 export default class HttpRequest {
-    private static readonly APIurl: string = process.env.API_URL || "http://127.0.0.1:8080/dbms/api";
+    private static readonly APIurl: string = "https://dbms-izemaghilas.herokuapp.com/api";
 
     public static send(arg: IHttpRequest): void {
-        
         let url: string = "";
         if(arg.relation){
             url = HttpRequest.APIurl+"/"+arg.resource+"?relation="+arg.relation;
